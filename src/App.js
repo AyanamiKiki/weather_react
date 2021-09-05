b8cded918069d1d651277191a14bde01be228744
AyanamiKiki <ayanami.kiki@gmail.com>
2021-09-05 17:37:59 +0200

    Footer formatted

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Simple Weather App</h1>
       <Weather />
-      <p>Open Source code by: <a href="https://github.com/AyanamiKiki/weather_react" target='_blank' rel="noreferrer">Ayanami Kiki</a> </p>
+      <p className="footer">Open Source code by: <a href="https://github.com/AyanamiKiki/weather_react" target='_blank' rel="noreferrer">Ayanami Kiki</a> </p>
     </div>
   );
 }
